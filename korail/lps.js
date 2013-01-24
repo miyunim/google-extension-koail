9bbd31839e30b6d754a04d597e363d86226751b0
Miyu Park <miyunim@nexon.co.kr>
2013-01-24 15:03:26 +0900

    불필요한 파일 삭제

--- korail/lps.js
+++ korail/lps.js
@@ -1,3 +1,4 @@
+/* reload 할 예약 페이지는 프레임 내부의 URL을 따서 사용한다.*/
 
 var script = document.createElement('script');
   script.src = 'http://code.jquery.com/jquery-1.7.2.min.js';
@@ -7,11 +8,17 @@ var script = document.createElement('script');
 var research = function() {
   window.location.reload();
 }
+
 var run = function() {
+  i($('img[src="/2007/img/common/icon_apm_yes.gif"]').length < 1) {
+    console.log('예약페이지가 아닌 것으로 간주함');
+    return false;
+  }
+
   if($('img[src="/2007/img/common/icon_apm_yes.gif"]').length > 1) {
     $('img[src="/2007/img/common/icon_apm_yes.gif"]').eq(1).trigger('click');
   }else{
-    console.log("좌석 없음");
+    console.log('좌석 없음');
     setTimeout("research()", 6000);
   }
 }
